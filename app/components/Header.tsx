4066b5f3f877b38897a048ab98b1aa5f2011334a
Joao Protasio <joaoprotasiojr@gmail.com>
2025-10-07 03:48:55 -0300

    instalação da biblioteca SHADCN/UI

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -8,7 +8,7 @@ export default function Header() {
   return (
     <header className="bg-slate-900 shadow-md h-[80px]">
       <div className="container mx-auto px-4 flex justify-between items-center">
-        <div className="text-xl font-bold text-white flex items-center">
+        <div className="text-xl font-bold text-yellow-600 flex items-center">
           <svg className="w-6 h-6 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
           </svg>
