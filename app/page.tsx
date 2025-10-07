08c04281c429a090de1afa9141882f5e89aa669f
Joao Protasio <joaoprotasiojr@gmail.com>
2025-10-07 01:42:34 -0300

    cor do botao contato yellow 600

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
         <div className="text-left text-white max-w-md ml-8">
           <h1 className="text-4xl font-bold mb-4">Bem-vindo ao Site de João Protásio Jr</h1>
           <p className="text-lg mb-8">Desenvolvedor web apaixonado por criar soluções inovadoras.</p>
-          <a href="/contact" className="bg-blue-600 text-white px-6 py-3 rounded-lg hover:bg-blue-700">Entre em Contato</a>
+          <a href="/contact" className="bg-yellow-600 text-white px-6 py-3 rounded-lg hover:bg-yellow-700">Entre em Contato</a>
         </div>
       </section>
     </main>
