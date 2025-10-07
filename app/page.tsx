68010d2802cd8b3f33f5d0e134fbdcb366b722e7
Joao Protasio <joaoprotasiojr@gmail.com>
2025-10-07 02:31:57 -0300

    ajuste nas cores dos cards

--- app/page.tsx
+++ app/page.tsx
@@ -10,27 +10,27 @@ export default function Home() {
           <a href="/contact" className="bg-yellow-600 text-white px-6 py-3 rounded-lg hover:bg-yellow-700"><Phone className="inline w-5 h-5 mr-2" />Entre em Contato</a>
         </div>
       </section>
-      <section className="py-16 bg-gray-100">
+      <section className="py-16 bg-slate-700">
         <div className="max-w-6xl mx-auto px-4">
           <h2 className="text-3xl font-bold text-center mb-12">Nossos Serviços</h2>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
-            <div className="bg-white p-6 rounded-lg shadow-md text-center">
-              <Globe className="w-12 h-12 mx-auto mb-4 text-blue-600" />
+            <div className="bg-slate-600 p-6 rounded-lg shadow-md text-center">
+              <Globe className="w-12 h-12 mx-auto mb-4 text-yellow-600" />
               <h3 className="text-xl font-semibold mb-2">Páginas Web</h3>
               <p className="text-gray-600">Desenvolvimento de Páginas Web</p>
             </div>
-            <div className="bg-white p-6 rounded-lg shadow-md text-center">
-              <Users className="w-12 h-12 mx-auto mb-4 text-green-600" />
+            <div className="bg-slate-600 p-6 rounded-lg shadow-md text-center">
+              <Users className="w-12 h-12 mx-auto mb-4 text-yellow-600" />
               <h3 className="text-xl font-semibold mb-2">Redes Socias</h3>
               <p className="text-gray-600">Conteudo para redes sociais</p>
             </div>
-            <div className="bg-white p-6 rounded-lg shadow-md text-center">
-              <Box className="w-12 h-12 mx-auto mb-4 text-purple-600" />
+            <div className="bg-slate-600 p-6 rounded-lg shadow-md text-center">
+              <Box className="w-12 h-12 mx-auto mb-4 text-yellow-600" />
               <h3 className="text-xl font-semibold mb-2">Modelo 3D</h3>
               <p className="text-gray-600">Produtos em 3D para divulgação</p>
             </div>
-            <div className="bg-white p-6 rounded-lg shadow-md text-center">
-              <Video className="w-12 h-12 mx-auto mb-4 text-red-600" />
+            <div className="bg-slate-600 p-6 rounded-lg shadow-md text-center">
+              <Video className="w-12 h-12 mx-auto mb-4 text-yellow-600" />
               <h3 className="text-xl font-semibold mb-2">Vídeos</h3>
               <p className="text-gray-600">Edição de vídeo para redes sociais</p>
             </div>
